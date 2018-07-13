3c423686dcdf5b60533aeb3954732847a1894d8d
josel120 <josegomez120@gmail.com>
2018-07-13 13:00:52 -0500

    pagna no encontrada

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+
 
 const sincronizarRoutes = require('./api/routes/sincronizar');
 app.use('/sincronizar', sincronizarRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+app.use(morgan('dev'));
 module.exports = app;
