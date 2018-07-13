0499e0682cef21de967b690a1c38ce78ca0dd14f
josel120 <josegomez120@gmail.com>
2018-07-13 18:52:23 -0500

    se agrego, buscarporID, y Editar

--- api/routes/sincronizar.js
+++ api/routes/sincronizar.js
@@ -80,6 +80,46 @@ router.delete("/:id", (req, res, next) => {
           error: err
         });
       });
-  });
+});
+
+router.get("/:id", (req, res, next) => {
+    const id = req.params.id;
+    FilesAndFolders.findById(id)
+      .exec()
+      .then(doc => {
+        console.log("From database", doc);
+        if (doc) {
+          res.status(200).json(doc);
+        } else {
+          res
+            .status(404)
+            .json({ message: "No valid entry found for provided ID" });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
+});
+
+router.patch("/:id", (req, res, next) => {
+    const id = req.params.id;
+    const updateOps = {};
+    for (const ops of req.body) {
+      updateOps[ops.propName] = ops.value;
+    }
+    FilesAndFolders.update({ _id: id }, { $set: updateOps })
+      .exec()
+      .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+});
 
 module.exports = router;
